2f3a7f53d0c2af341aacd8444ff359be16f5c39f
senatecnologia@outlook.com <dti94395122>
2020-02-17 17:37:55 -0300

    Update carregar_dados.sh

--- carregar_dados.sh
+++ carregar_dados.sh
@@ -13,4 +13,6 @@ echo "Carregando dados da aplicação ..."
 
 echo "      ...executando o script SQL para CARGA DO DOMÍNIO DE DADOS..."
 
-pg_restore -h $HOSTNAMELINK -p $PORT -U $DBUSERNAME --no-password -d $DBNAME /opt/e-SUS/jboss-as-7.2.0.Final/dump.backup
+#pg_restore -h $HOSTNAMELINK -p $PORT -U $DBUSERNAME --no-password -d $DBNAME /opt/e-SUS/jboss-as-7.2.0.Final/dump.backup
+
+docker exec -it db_postgre pg_restore -h localhost -p 5433 -U postgres --no-password -d esus /opt/e-SUS/jboss-as-7.2.0.Final/dump.backup
